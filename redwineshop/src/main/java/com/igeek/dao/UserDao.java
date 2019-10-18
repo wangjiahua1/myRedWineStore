1aad3cff9d4612f4b3542094ab99f7c23308e571
卢翔 <1325817291@qq.com>
2019-10-18 16:26:29 +0800

    test

--- redwineshop/src/main/java/com/igeek/dao/UserDao.java
+++ redwineshop/src/main/java/com/igeek/dao/UserDao.java
@@ -1,13 +1,9 @@
 package com.igeek.dao;
 
 import com.igeek.domain.User;
-import org.apache.commons.dbutils.handlers.BeanHandler;
-
-import java.sql.SQLException;
 
 public interface UserDao {
-    User findUser(User user);
-    public boolean checkActiveCode(String activeCode) ;
-    public void updateActiveCode(String activeCode) ;
-    public void save(User user) ;
+    User findUserByEmail(User user);
+
+    User findUserByUsername(User user);
 }
--- redwineshop/src/main/java/com/igeek/dao/imp/UserDaoImpl.java
+++ redwineshop/src/main/java/com/igeek/dao/imp/UserDaoImpl.java
@@ -8,12 +8,24 @@ import org.apache.commons.dbutils.handlers.BeanHandler;
 
 import java.sql.SQLException;
 
-public class UserDaoImpl implements UserDao{
+public class UserDaoImpl implements UserDao {
     QueryRunner qr=new QueryRunner(JDBCTools.getDataSource());
     String sql="";
     @Override
-    public User findUser(User user) {
-        sql="select * from user where username = ? and password = ? and activeState =1";
+    public User findUserByEmail(User user) {
+        sql="select * from user where email = ? and password = ?";
+        User u=null;
+        try {
+            u=qr.query(sql,new BeanHandler<>(User.class),user.getEmail(),user.getPassword());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return u;
+    }
+
+    @Override
+    public User findUserByUsername(User user) {
+        sql="select * from user where username = ? and password = ?";
         User u=null;
         try {
             u=qr.query(sql,new BeanHandler<>(User.class),user.getUsername(),user.getPassword());
@@ -21,33 +33,5 @@ public class UserDaoImpl implements UserDao{
             e.printStackTrace();
         }
         return u;
-    }
-    public boolean checkActiveCode(String activeCode) {
-        String sql = "select * from user where code = ?";
-        User user = null;
-        try {
-            user = qr.query(sql,new BeanHandler<>(User.class),activeCode);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return user!=null?true:false;
-    }
-
-    public void updateActiveCode(String activeCode) {
-        String  sql ="update user set state = 1 where code = ?";
-        try {
-            qr.update(sql,activeCode);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void save(User user) {//INSERT INTO table_name (列1, 列2,...) VALUES (值1, 值2,....)
-        String sql="insert into user(uid,email,password,code) values(?,?,?,?)";
-        try {
-            qr.update(sql,user.getUid(),user.getEmail(),user.getPassword(),user.getCode());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
